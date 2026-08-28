c64b5499df93d171ec5e3b23bceb0d0de5b56c14
varasahasra8-byte <varasahasra8@gmail.com>
2026-08-28 23:50:25 +0530

    q3

---
+++ OneDrive/Desktop/step_sem3/q3.java
@@ -0,0 +1,40 @@
+public class q3
+{
+    static void findLongestStreak(String signalLog)
+    {
+        int currentCount = 1;
+        int longestCount = 1;
+        char longestColor = signalLog.charAt(0);
+
+        for(int i = 1; i < signalLog.length(); i++)
+        {
+            if(signalLog.charAt(i) == signalLog.charAt(i - 1))
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentCount = 1;
+            }
+
+            if(currentCount > longestCount)
+            {
+                longestCount = currentCount;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak Color: " + longestColor);
+        System.out.println("Longest Streak Length: " + longestCount);
+    }
+
+    public static void main(String[] args)
+    {
+        String signalLog = "RRGGGYRR";
+
+        System.out.println("Traffic Signal Result");
+        findLongestStreak(signalLog);
+    }
+}
+    
+
